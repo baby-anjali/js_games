c822612ad4dc71d1d5fe6c46a161d66e50057e04
baby-anjali <anjaleena2783@gmail.com>
2024-11-22 18:15:28 +0530

    Update snake_and_ladder.js

--- snake_and_ladder.js
+++ snake_and_ladder.js
@@ -1,67 +1,101 @@
 const welcomeMessage = '*----------Snake & Ladder----------* \n' +
-  '<<-<---🐍🐍🐍---🎲---🪜🪜🪜----->->>';
+  '<<-<---🐍🐍🐍---🎲---🪜🪜🪜----->->>\n' +
+  'Let\'s start! 🏁🏁🏁\n\n';
 
-const borderUp = '╒════════╕\n'
-const borderDown = '\n╘════════╛'
-
-const one =  borderUp + '|⚫️ ⚫️ ⚫️|\n|⚫️ ⚪️ ⚫️|\n|⚫️ ⚫️ ⚫️|'  + borderDown;
-const two = borderUp + '|⚪️ ⚫️ ⚫️|\n' +  '|⚫️ ⚫️ ⚫️|\n|⚫️ ⚫️ ⚪️|'  + borderDown;
-const three = borderUp + '|⚫️ ⚫️ ⚪️|\n' +  '|⚫️ ⚪️ ⚫️|\n|⚪️ ⚫️ ⚫️|'  + borderDown;
-const four = borderUp + '|⚪️ ⚫️ ⚪️|\n' +  '|⚫️ ⚫️ ⚫️|\n|⚪️ ⚫️ ⚪️|'  + borderDown;
-const five = borderUp + '|⚪️ ⚫️ ⚪️|\n' +  '|⚫️ ⚪️ ⚫️|\n|⚪️ ⚫️ ⚪️|'  + borderDown;
-const six = borderUp + '|⚪️ ⚫️ ⚪️|\n' +  '|⚪️ ⚫️ ⚪️|\n|⚪️ ⚫️ ⚪️|'  + borderDown;
-  
 const tabs = '\t\t\t';
-let winner = '';
+
+let player1 = '';
+let player2 = '';
 
 let player1Position = 0;
 let player2Position = 0;
 
+const borderUp = '╒════════╕'
+const borderDown = '╘════════╛'
+
+const snakeComment1 = "Oh no!😱 A snake has bitten "
+const snakeComment2 = "...🐍 You're going down🥲⬇️ ";
+const ladderComment1 = "Woah!😳 ";
+const ladderComment2 = " got a ladder...🪜 You're climbing up😄⬆️";
+
+function congratulate(winner) {
+  console.log('Woohoo!!!', winner, 'has won!🥳🎉🎊');  
+}
+
+function addBorders(string) {
+  return '┃' + string + '┃';
+}
+
+const EMPTY = addBorders('⚫️ ⚫️ ⚫️');
+const TWO_WHITES = addBorders('⚪️ ⚫️ ⚪️');
+const LEFT_WHITE = addBorders('⚪️ ⚫️ ⚫️');
+const RIGHT_WHITE = addBorders('⚫️ ⚫️ ⚪️');
+const MIDDLE_WHITE = addBorders('⚫️ ⚪️ ⚫️');
+
 function wait(limit) {
   for (let i = 0; i < limit; i++);
-  return;
 }
 
-function printDice(number) {
+function joinLines(first, second, third) {
+  return first + '\n' + second + '\n' + third;
+}
+
+function getFace(number) {
   switch (number) {
-    case 1: return one;
-    case 2: return two;
-    case 3: return three;
-    case 4: return four;
-    case 5: return five;
-    case 6: return six;
+    case 1:
+      return joinLines(EMPTY, MIDDLE_WHITE, EMPTY);
+    case 2:
+      return joinLines(LEFT_WHITE, EMPTY, RIGHT_WHITE);
+    case 3:
+      return joinLines(LEFT_WHITE, MIDDLE_WHITE, RIGHT_WHITE);
+    case 4:
+      return joinLines(TWO_WHITES, EMPTY, TWO_WHITES);
+    case 5:
+      return joinLines(TWO_WHITES, MIDDLE_WHITE, TWO_WHITES);
+    case 6:
+      return joinLines(TWO_WHITES, TWO_WHITES, TWO_WHITES);
   }
 }
 
-function diceToggle(dice) {
-  console.clear();
-  console.log(dice);
-  wait(8 ** 9);
-  return;
+function getDiceFace(number) {
+  return joinLines(borderUp, getFace(number), borderDown);
 }
 
-function visualDiceRoll() {
-  for (let i = 0; i < 5; i++) {
-    diceToggle(one);
-    diceToggle(two);
-    diceToggle(three);
-    diceToggle(four);
-    diceToggle(five);
-    diceToggle(six);
+function animateDiceRoll() {
+  for (let j = 0; j < 5; j++) {
+    for (let i = 1; i < 7; i++) {
+      console.log(getDiceFace(i));
+      wait(8 ** 9);
+      console.clear();
+    }
   }
-
-  console.clear();
 }
 
-function scoreboard(player1, player2) {
+function endMessage(winner) {
+  console.log(winner, 'has won the game!🎊🎉🥳 Thank you for playing!!!');
+}
+
+function scoreboard(player1, player2){
   const scoreLine1 = '\n»»——⍟——⍟——⍟——⍟——⍟ Current positions ⍟——⍟——⍟——⍟——⍟——««';
   let scoreLine2 = '\n\n\t' + player1 + " : " + player1Position + tabs;
   let scoreLine3 = player2 + " : " + player2Position + "\n";
-  let scoreLine4 = '\n»»——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——««';
-
+  let scoreLine4 = '\n»»——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——⍟——««\n';
+  
   console.log(scoreLine1 + scoreLine2 + scoreLine3 + scoreLine4);
+}
 
-  return;
+function getRandomInt() {
+  return Math.ceil(Math.random() * 6);
+}
+
+function getName(text) {
+  const name = prompt("Enter " + text + " player's name: ");
+  
+  if(name !== '') {
+    return name;
+  }
+  
+  return getName(text);
 }
 
 function onSnake(position) {
@@ -89,95 +123,74 @@ function onLadder(position) {
   }
 }
 
-function rollDice(player) {
-  prompt(player + "'s turn to roll the dice");
-  const diceValue = Math.ceil(Math.random() * 6);
-  visualDiceRoll();
-  console.log(printDice(diceValue));
-  console.log('\n' + player + ' got ' + diceValue + '!');
-  
-  return diceValue;
+function getComments(player, newPosition, currentPosition) {
+  if (newPosition < currentPosition) {
+    return snakeComment1 + player + snakeComment2;
+  }
+
+  if (newPosition > currentPosition) {
+    return ladderComment1 + player + ladderComment2;
+  }
+
+  return '';
 }
 
-function reached100(player1, player2) {
-  if (player1Position === 100 || player2Position === 100) {
-    winner = player1Position === 100 ? player1 : player2;
-  }
-  
-  return winner !== '';
+function isOnSnakeOrLadder (position) {
+  const newPosition = onSnake(position);
+  return onLadder(newPosition);
 }
 
 function playerMove(player, position, diceValue) {
+  const currentPosition = position + diceValue;
+  
+  if (currentPosition > 100) {
+    console.log('You need exact number to home or lesser number to move ahead');
+    
+    return position;
+  }
+  
+  const newPosition = isOnSnakeOrLadder(currentPosition);
 
-  position = position + diceValue;
+  console.log(getComments(player, newPosition, currentPosition));
+  return newPosition;
+}
+
+function turnToPlay(player, position) {
+  scoreboard(player1, player2);
+  prompt(player, "'s turn to play");
+
+  animateDiceRoll();
+  const diceValue = getRandomInt();
   
-  if (position > 100) {
-    return position - diceValue;
-  }
+  console.log(getDiceFace(diceValue));
+  console.log(player, 'got a ', diceValue);
   
-  let newPosition = onSnake(position);
-  if (newPosition !== position) {
-    console.log("Oh no!😱 A snake has bitten " + player + "...🐍 You're going down🥲⬇️ \n");
-    return newPosition;
-  }
-  
-  newPosition = onLadder(position);
-  if (newPosition !== position) {
-    console.log("\nWoah!😳 " + player + " got a ladder...🪜 " + player + " is climbing up😄⬆️\n");
-    return newPosition;
-  }
-  
+  position = playerMove(player, position, diceValue);
   return position;
 }
 
-function playerRoll(player, position) {
-  let playerDice = rollDice(player);
-  
-  return playerMove(player, position, playerDice);
-}
-
 function startGame(player1, player2) {
-  console.log();
-  player1Position = playerRoll(player1, player1Position);
-  scoreboard(player1, player2);
-  
-  if (reached100(player1, player2)) {
-    return;
+  while (player2Position < 100) {
+    player1Position = turnToPlay(player1, player1Position);
+    
+    if(player1Position === 100) {
+      return player1;
+    }
+    
+    player2Position = turnToPlay(player2, player2Position);
   }
-  
-  console.log();
-  player2Position = playerRoll(player2, player2Position);
-  scoreboard(player1, player2);
-  
-  if (reached100(player1, player2)) {
-    return;
-  }
+
+  return player2;  
 }
 
-function isPlaying() {
-  console.log(welcomeMessage + '\n');
-
-  if (!confirm('Do you want to play?')) {
-    console.log('Ok, bye!');
-    return;
-  }
-
-  const player1 = prompt("Enter first player's name: ", "Sam");
-  const player2 = prompt("Enter second player's name: ", "Julie");
-
-  prompt('Game start!\t🔴-🟡-🟢\n(press enter)');
-  console.clear();
-
-  scoreboard(player1, player2);
-
-  while (winner === '') {
-    startGame(player1, player2);    
-  }
-
-  console.clear();
-  const loser = winner === player1 ? player2 : player1;
-  console.log('\n' + winner + ' has won the game!🥳🎉🎊\n' + loser + ' lost...😭🥀');
-  console.log('Thank you staying here. Have a nice day!😁\n');
+function loadGame() {
+  console.log(welcomeMessage);
+  
+  player1 = getName('first');
+  player2 = getName('second');
+  
+  const winner = startGame(player1, player2);
+  congratulate(winner);
 }
 
-isPlaying();
+loadGame();
